6a5ee51ae6b2314574a17976fbe0fa24a6f14af3
Dilum Darshana <dilum.dar@gmail.com>
2024-01-09 11:40:14 +0530

    Cleaned old files

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Router from './router';
-import './styles.scss';
-
-ReactDOM.render(<Router />, document.getElementById('app'));
